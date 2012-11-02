6829c9a6ee233926fa15b33102e5996c46497e70
Nathan Smith <nsmith13@nd.edu>
2012-11-01 21:22:35 -0400

    first commit

---
+++ obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,139 @@
+obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+ jni/DetectionBasedTracker_jni.cpp jni/DetectionBasedTracker_jni.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/jni.h \
+ /Users/nathansmith/dev/ComputerVisionCourse/semesterProject/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+ /Users/nathansmith/dev/ComputerVisionCourse/semesterProject/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/assert.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdlib.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/string.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/malloc.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/alloca.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/strings.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/types.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdint.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/_types.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/_types.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/posix_types.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/stddef.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/compiler.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/posix_types.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/types.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/types.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/kernel.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/memory.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/math.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/limits.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/limits.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/limits.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/internal_types.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/limits.h \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/syslimits.h \
+ /Users/nathansmith/dev/ComputerVisionCourse/semesterProject/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ /Users/nathansmith/Android/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm \
+ /Users/nathansmith/Android/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath \
+ /Users/nathansmith/Android/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef \
+ /Users/nathansmith/Android/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/complex \
+ /Users/nathansmith/Android/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/map \
+ /Users/nathansmith/Android/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/vector \
+ /Users/nathansmith/dev/ComputerVisionCourse/semesterProject/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+ /Users/nathansmith/Android/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/limits \
+ /Users/nathansmith/dev/ComputerVisionCourse/semesterProject/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ /Users/nathansmith/dev/ComputerVisionCourse/semesterProject/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp \
+ /Users/nathansmith/dev/ComputerVisionCourse/semesterProject/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect.hpp \
+ /Users/nathansmith/Android/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/deque \
+ /Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/android/log.h
+
+jni/DetectionBasedTracker_jni.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/jni.h:
+
+/Users/nathansmith/dev/ComputerVisionCourse/semesterProject/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+
+/Users/nathansmith/dev/ComputerVisionCourse/semesterProject/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/assert.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdlib.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/string.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/malloc.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/alloca.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/strings.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/types.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdint.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/_types.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/_types.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/stddef.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/compiler.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/types.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/types.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/kernel.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/memory.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/math.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/limits.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/limits.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/limits.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/limits.h:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/nathansmith/dev/ComputerVisionCourse/semesterProject/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+/Users/nathansmith/Android/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm:
+
+/Users/nathansmith/Android/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath:
+
+/Users/nathansmith/Android/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef:
+
+/Users/nathansmith/Android/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/complex:
+
+/Users/nathansmith/Android/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/map:
+
+/Users/nathansmith/Android/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/vector:
+
+/Users/nathansmith/dev/ComputerVisionCourse/semesterProject/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/Users/nathansmith/Android/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/limits:
+
+/Users/nathansmith/dev/ComputerVisionCourse/semesterProject/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+/Users/nathansmith/dev/ComputerVisionCourse/semesterProject/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp:
+
+/Users/nathansmith/dev/ComputerVisionCourse/semesterProject/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect.hpp:
+
+/Users/nathansmith/Android/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/deque:
+
+/Users/nathansmith/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/android/log.h:
