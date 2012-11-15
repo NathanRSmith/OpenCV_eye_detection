33a53e5ae90f6bbe6409d17024727c0d7e176f5d
Nathan Smith <nsmith13@nd.edu>
2012-11-14 23:37:32 -0500

    working on processing individual eyes

--- python_investigation/faceEyeDetection.py
+++ python_investigation/faceEyeDetection.py
@@ -34,7 +34,7 @@ USAGE: facedetect.py [--facecascade <cascade_fn>] [--eyescascade-facecascade <ca
 '''
 
 def detect(img, cascade):
-    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30), flags = cv.CV_HAAR_SCALE_IMAGE)
+    rects = cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=4, minSize=(30, 30), flags = cv.CV_HAAR_SCALE_IMAGE)
     if len(rects) == 0:
         return []
     rects[:,2:] += rects[:,:2]
@@ -69,6 +69,96 @@ def find_rect_center(rect):
     y = (y1 + y2) / 2
     return (x, y)
 
+def combineEyes(left, right):
+    # stack vert
+    
+    h = max(left.shape[0], right.shape[0])
+    w = left.shape[1] + right.shape[1]
+    hoff = left.shape[0]
+    
+    #pdb.set_trace()
+    
+    shape = list(left.shape)
+    shape[0] = h
+    shape[1] = w
+    
+    comb = np.zeros(tuple(shape),left.dtype)
+    
+    # left will be on left, aligned top, with right on right
+    comb[:left.shape[0],:left.shape[1]] = left
+    comb[:right.shape[0],left.shape[1]:] = right
+    
+    return comb
+
+def processFrame(img, facecascade, eyescascade):
+    """Process a single frame.
+    """
+    
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.equalizeHist(gray)
+    
+    rects = detect(gray, facecascade)
+    vis = img.copy()
+    draw_rects(vis, rects, (0, 255, 0))
+    
+    eyesubrectsleft, eyesubrectsright = [], []
+    
+    for x1, y1, x2, y2 in rects:
+        roileft = gray[y1:(y1+y2)/2, x1:(x1+x2)/2]
+        vis_roileft = vis[y1:(y1+y2)/2, x1:(x1+x2)/2]
+        gray_roileft = gray[y1:(y1+y2)/2, x1:(x1+x2)/2]
+        eyesubrectsleft = detect(roileft.copy(), eyescascade)
+        draw_rects(vis_roileft, eyesubrectsleft, (255, 0, 0))
+        
+        #pdb.set_trace()
+        
+        roiright = gray[y1:(y1+y2)/2, (x1+x2)/2:x2]
+        vis_roiright = vis[y1:(y1+y2)/2, (x1+x2)/2:x2]
+        gray_roiright = gray[y1:(y1+y2)/2, (x1+x2)/2:x2]
+        eyesubrectsright = detect(roiright.copy(), eyescascade)
+        draw_rects(vis_roiright, eyesubrectsright, (255, 0, 0))
+        
+        
+        if len(eyesubrectsleft) > 0 and len(eyesubrectsright) > 0:
+            left = eyesubrectsleft[0] 
+            right = eyesubrectsright[0]
+            
+            comb = combineEyes(
+                                gray_roileft[left[1]:left[3], left[0]:left[2]],
+                                gray_roiright[right[1]:right[3], right[0]:right[2]]
+                                )
+            cv2.imshow('combinedeyes', comb)
+            
+            leftthresh = gray_roileft[left[1]:left[3], left[0]:left[2]].copy()
+            cv2.adaptiveThreshold(leftthresh, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 99, 5, leftthresh)
+            
+            rightthresh = gray_roiright[right[1]:right[3], right[0]:right[2]].copy()
+            cv2.adaptiveThreshold(rightthresh, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 99, 5, rightthresh)
+            
+            combthresh = combineEyes(leftthresh, rightthresh)
+            cv2.imshow('combinedthresh', combthresh)
+        
+        #pdb.set_trace()
+
+#	    mouthsubrects = detect(roi.copy(), mouthcascade)
+#	    
+#	    # remove mouth detections that overlap with eyes
+#	    remove = []
+#	    if type(mouthsubrects) != list:
+#		for i in range(mouthsubrects.shape[0]):
+#		    for rect in eyesubrects:
+#			if is_intersection(mouthsubrects[i],rect):
+#			    remove.append(i)
+#		for i in sorted(set(remove), reverse=True):
+#		    mouthsubrects = np.delete(mouthsubrects, (i), axis=0)
+#		
+#            draw_rects(vis_roi, mouthsubrects, (0, 0, 255))
+
+    cv2.imshow('facedetect', vis)
+    
+    return vis, gray, rects, eyesubrectsleft, eyesubrectsright
+
+
 if __name__ == '__main__':
     import sys, getopt
     print help_message
@@ -85,80 +175,70 @@ if __name__ == '__main__':
     facecascade = cv2.CascadeClassifier(cascade_fn)
     eyescascade = cv2.CascadeClassifier(eyes_fn)
     mouthcascade = cv2.CascadeClassifier(mouth_fn)
-
-# assume camera 0, webcam. Could also be a file name, etc.
-
-    cam = cv2.VideoCapture(0)
-
-    fileindex = 0
-
-    while True:
-        ret, img = cam.read()
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        gray = cv2.equalizeHist(gray)
+    
+    
+    mode = 'picture'
+    #mode = 'video'
+    #source = 'imgs/IMG_20121112_121434.jpg'
+    #source = 'imgs/IMG_20121112_121455.jpg'
+    #source = 'imgs/IMG_20121112_121522.jpg'
+    #source = 'imgs/IMG_20121112_121443.jpg'
+    #source = 'imgs/IMG_20121112_121501.jpg'
+    #source = 'imgs/IMG_20121112_121525.jpg'
+    #source = 'imgs/IMG_20121112_121448.jpg'
+    #source = 'imgs/IMG_20121112_121519.jpg'
+    source = 'imgs/2.png'
+    
+    if mode == 'picture':
+        img = cv2.imread(source)
+        processFrame(img, facecascade, eyescascade)
         
-        rects = detect(gray, facecascade)
-        vis = img.copy()
-        draw_rects(vis, rects, (0, 255, 0))
-        for x1, y1, x2, y2 in rects:
-            roileft = gray[y1:(y1+y2)/2, x1:(x1+x2)/2]
-            vis_roileft = vis[y1:(y1+y2)/2, x1:(x1+x2)/2]
-            eyesubrectsleft = detect(roileft.copy(), eyescascade)
-            draw_rects(vis_roileft, eyesubrectsleft, (255, 0, 0))
-            
-            #pdb.set_trace()
-            
-            roiright = gray[y1:(y1+y2)/2, (x1+x2)/2:x2]
-            vis_roiright = vis[y1:(y1+y2)/2, (x1+x2)/2:x2]
-            eyesubrectsright = detect(roiright.copy(), eyescascade)
-            draw_rects(vis_roiright, eyesubrectsright, (255, 0, 0))
-	    
-	    #pdb.set_trace()
-	    
-#	    mouthsubrects = detect(roi.copy(), mouthcascade)
-#	    
-#	    # remove mouth detections that overlap with eyes
-#	    remove = []
-#	    if type(mouthsubrects) != list:
-#		for i in range(mouthsubrects.shape[0]):
-#		    for rect in eyesubrects:
-#			if is_intersection(mouthsubrects[i],rect):
-#			    remove.append(i)
-#		for i in sorted(set(remove), reverse=True):
-#		    mouthsubrects = np.delete(mouthsubrects, (i), axis=0)
-#		
-#            draw_rects(vis_roi, mouthsubrects, (0, 0, 255))
-
-        cv2.imshow('facedetect', vis)
+        pdb.set_trace()
+    
+    elif mode == 'video':
         
-	key = cv2.waitKey(1) & 0xff
-
-	if key == ord('w'):
-		cv2.imwrite(str(fileindex)+'.png',img)
-		cv2.imwrite(str(fileindex)+'.annotated.png',vis)
-		rindex = 0 
-		if (len(rects) == 0) and (len(eyesubrects) == 0) and (len(mouthsubrects) == 0):
-			continue
-		#else
-		f = file(str(fileindex)+'.txt','w')
-		print >>f, 'For image file '+str(fileindex)+'.png:'
-		for x1,y1,x2,y2 in rects:
-			print >>f,'face rect ',rindex,': (',x1,',',y1,') -> (',x2,',',y2,'), ',find_rect_center((x1,y1,x2,y2))
-			rindex = rindex +1
-		rindex = 0
-		for x1,y1,x2,y2 in eyesubrects:
-			print >>f,'eye rect ',rindex,': (',x1,',',y1,') -> (',x2,',',y2,'), ',find_rect_center((x1,y1,x2,y2))
-			rindex = rindex + 1
-		rindex = 0
-		for x1,y1,x2,y2 in mouthsubrects:
-			print >>f,'mouth rect ',rindex,': (',x1,',',y1,') -> (',x2,',',y2,'), ',find_rect_center((x1,y1,x2,y2))
-			rindex = rindex + 1
-		f.close()
-		fileindex = fileindex + 1
-	elif key == 27:
-		break
-	else:
-		continue
+        # assume camera 0, webcam. Could also be a file name, etc.
+        cam = cv2.VideoCapture(0)
+    
+        fileindex = 0
+    
+        while True:
+            ret, img = cam.read()
+            
+            vis, gray, rects, eyesubrectsleft, eyesubrectsright = processFrame(img, facecascade, eyescascade)
+        
+            key = cv2.waitKey(1) & 0xff
+        
+            if key == ord('w'):
+                cv2.imwrite(str(fileindex)+'.png',img)
+                cv2.imwrite(str(fileindex)+'.annotated.png',vis)
+                rindex = 0 
+                if (len(rects) == 0) and (len(eyesubrects) == 0) and (len(mouthsubrects) == 0):
+                        continue
+                #else
+                f = file(str(fileindex)+'.txt','w')
+                print >>f, 'For image file '+str(fileindex)+'.png:'
+                for x1,y1,x2,y2 in rects:
+                        print >>f,'face rect ',rindex,': (',x1,',',y1,') -> (',x2,',',y2,'), ',find_rect_center((x1,y1,x2,y2))
+                        rindex = rindex +1
+                rindex = 0
+                for x1,y1,x2,y2 in eyesubrectsleft:
+                        print >>f,'left eye rect ',rindex,': (',x1,',',y1,') -> (',x2,',',y2,'), ',find_rect_center((x1,y1,x2,y2))
+                        rindex = rindex + 1
+                rindex = 0
+                for x1,y1,x2,y2 in eyesubrectsright:
+                        print >>f,'right eye rect ',rindex,': (',x1,',',y1,') -> (',x2,',',y2,'), ',find_rect_center((x1,y1,x2,y2))
+                        rindex = rindex + 1
+                #rindex = 0
+                #for x1,y1,x2,y2 in mouthsubrects:
+                #        print >>f,'mouth rect ',rindex,': (',x1,',',y1,') -> (',x2,',',y2,'), ',find_rect_center((x1,y1,x2,y2))
+                #        rindex = rindex + 1
+                f.close()
+                fileindex = fileindex + 1
+            elif key == 27:
+                break
+            else:
+                continue
 
     cv2.destroyAllWindows()
 
