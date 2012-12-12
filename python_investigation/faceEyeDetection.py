60e6ec664c87eea6498d8ea206a168c06ff5109c
Nathan Smith <nsmith13@nd.edu>
2012-12-11 23:13:38 -0500

    successfully fitting ellipse to eye area. Not great but it is a start

--- python_investigation/faceEyeDetection.py
+++ python_investigation/faceEyeDetection.py
@@ -42,15 +42,19 @@ USAGE: facedetect.py [--facecascade <cascade_fn>] [--eyescascade-facecascade <ca
 
 
 def detect(img, cascade):
-    rects = cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=4, minSize=(30, 30), flags = cv.CV_HAAR_SCALE_IMAGE)
-    if len(rects) == 0:
+    """Detect the cascade feature.
+    """
+    facerects = cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=4, minSize=(30, 30), flags = cv.CV_HAAR_SCALE_IMAGE)
+    if len(facerects) == 0:
         return []
-    rects[:,2:] += rects[:,:2]
-    return rects
+    facerects[:,2:] += facerects[:,:2]
+    return facerects
 
-def draw_rects(img, rects, color):
+def draw_rects(img, facerects, color):
+    """Draw all rects in the list.
+    """
     tag = 0
-    for x1, y1, x2, y2 in rects:
+    for x1, y1, x2, y2 in facerects:
         cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
         cv2.putText(img,str(tag),(x1,y2),cv2.FONT_HERSHEY_PLAIN,2.0,(200,0,200),2)
         tag = tag+1
@@ -77,14 +81,18 @@ def find_rect_center(rect):
     y = (y1 + y2) / 2
     return (x, y)
 
+def rectArea(rect):
+    """Get area of the supplied rectangle.
+    """
+    x1, y1, x2, y2 = rect
+    return (x2-x1) * (y2-y1)
+
 def combineEyes(left, right):
-    # stack vert
-    
+    """Stack images horizontally.
+    """
     h = max(left.shape[0], right.shape[0])
     w = left.shape[1] + right.shape[1]
     hoff = left.shape[0]
-    
-    #pdb.set_trace()
     
     shape = list(left.shape)
     shape[0] = h
@@ -98,6 +106,38 @@ def combineEyes(left, right):
     
     return comb
 
+def processEye(eyesubrects, vis_roi, gray_roi):
+    """threshold
+    get contours
+    find largest
+    fit ellipse
+    """
+    # apply threshold
+    thresh = gray_roi.copy()
+    #cv2.adaptiveThreshold(leftthresh, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 25, 25, leftthresh)
+    thresh = thresholdByPercentage(thresh, .075)
+    
+    # find contours from thresholded img
+    contours, heirarchy = cv2.findContours(thresh.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    
+    maxarea = 0
+    maxidx = 0
+    
+    # find contour with largest area
+    for idx in range(len(contours)):
+        a = cv2.contourArea(contours[idx])
+        if maxarea < a:
+            maxarea = a
+            maxidx = idx
+    
+    cv2.drawContours(vis_roi, contours, maxidx, (0,0,255), -1)
+    
+    # fit ellipse to the detected contour
+    ellipseBox = cv2.fitEllipse(contours[maxidx])
+    cv2.ellipse(vis_roi, ellipseBox, (50, 150, 255))
+    
+    return thresh, ellipseBox
+
 def processFrame(img, facecascade, eyescascade):
     """Process a single frame.
     """
@@ -105,130 +145,138 @@ def processFrame(img, facecascade, eyescascade):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     gray = cv2.equalizeHist(gray)
     
-    rects = detect(gray, facecascade)
+    facerects = detect(gray, facecascade)
     vis = img.copy()
-    draw_rects(vis, rects, (0, 255, 0))
+    draw_rects(vis, facerects, (0, 255, 0))
     
+    facerect = None
     eyesubrectsleft, eyesubrectsright = [], []
+    eyesubrectleft, eyesubrectright = None, None
+    leftEllipseBox, rightEllipseBox = None, None
     
-    for x1, y1, x2, y2 in rects:
+    
+    try:
+        # find largest face rect and process
+        maxsize = 0
+        maxidx = 0
+        for i in range(len(facerects)):
+            a = rectArea(facerects[i])
+            if maxsize < a:
+                maxsize = a
+                maxidx = i
+        
+        facerect = facerects[maxidx]
+        x1, y1, x2, y2 = facerect
+        
+        # left eye
         roileft = gray[y1:(y1+y2)/2, x1:(x1+x2)/2]
         vis_roileft = vis[y1:(y1+y2)/2, x1:(x1+x2)/2]
         gray_roileft = gray[y1:(y1+y2)/2, x1:(x1+x2)/2]
         eyesubrectsleft = detect(roileft.copy(), eyescascade)
         draw_rects(vis_roileft, eyesubrectsleft, (255, 0, 0))
         
-        #pdb.set_trace()
-        
+        # right eye
         roiright = gray[y1:(y1+y2)/2, (x1+x2)/2:x2]
         vis_roiright = vis[y1:(y1+y2)/2, (x1+x2)/2:x2]
         gray_roiright = gray[y1:(y1+y2)/2, (x1+x2)/2:x2]
         eyesubrectsright = detect(roiright.copy(), eyescascade)
         draw_rects(vis_roiright, eyesubrectsright, (255, 0, 0))
         
-        
+        # if left and right eyes detected
         if len(eyesubrectsleft) > 0 and len(eyesubrectsright) > 0:
-            left = eyesubrectsleft[0] 
-            right = eyesubrectsright[0]
+            # take first eye detected on each side
+            eyesubrectleft = eyesubrectsleft[0] 
+            eyesubrectright = eyesubrectsright[0]
             
-            vis_roileft_eye = vis_roileft[left[1]:left[3], left[0]:left[2]]
-            vis_roiright_eye = vis_roiright[right[1]:right[3], right[0]:right[2]]
+            # slice from color img
+            vis_roileft_eye = vis_roileft[eyesubrectleft[1]:eyesubrectleft[3], eyesubrectleft[0]:eyesubrectleft[2]]
+            vis_roiright_eye = vis_roiright[eyesubrectright[1]:eyesubrectright[3], eyesubrectright[0]:eyesubrectright[2]]
             
-            gray_roileft_eye = gray_roileft[left[1]:left[3], left[0]:left[2]]
-            gray_roiright_eye = gray_roiright[right[1]:right[3], right[0]:right[2]]
+            # slice from gray img
+            gray_roileft_eye = gray_roileft[eyesubrectleft[1]:eyesubrectleft[3], eyesubrectleft[0]:eyesubrectleft[2]]
+            gray_roiright_eye = gray_roiright[eyesubrectright[1]:eyesubrectright[3], eyesubrectright[0]:eyesubrectright[2]]
             
+            # display combined gray eyes (pre-processed)
             comb = combineEyes(
                                 gray_roileft_eye,
                                 gray_roiright_eye
                                 )
             cv2.imshow('combinedeyes', comb)
             
-            #pdb.set_trace()
-            
-            circleleft = None
-            circleright = None
-            
-            circlesleft = cv2.HoughCircles(gray_roileft_eye, cv2.cv.CV_HOUGH_GRADIENT, 1, 10, param1=100, param2=25, minRadius=5)
-            if circlesleft != None:
-                circleleft = circlesleft[0,0]
-                for i in circlesleft[0,:]:
-                    cv2.circle(vis_roileft_eye,(i[0],i[1]),i[2],(0,255,0),1)  # draw the outer circle
-                    cv2.circle(vis_roileft_eye,(i[0],i[1]),2,(0,0,255),3)     # draw the center of the circle
-            
-            circlesright = cv2.HoughCircles(gray_roiright_eye, cv2.cv.CV_HOUGH_GRADIENT, 1, 10, param1=100, param2=25, minRadius=5)
-            if circlesright != None:
-                circleright = circlesright[0,0]
-                for i in circlesright[0,:]:
-                    cv2.circle(vis_roiright_eye,(i[0],i[1]),i[2],(0,255,0),1)  # draw the outer circle
-                    cv2.circle(vis_roiright_eye,(i[0],i[1]),2,(0,0,255),3)     # draw the center of the circle
-            
-            #if circleleft != None and circleright != None:
-            #    
-            #    #cv2.findContour
-            #    
-            #    #cv2.circle(gray_roileft_eye,(circleleft[0],circleleft[1]),circleleft[2],(0),-1)
-            #    #
-            #    #maskleft = np.zeros((gray_roileft_eye.shape[0]+2,gray_roileft_eye.shape[1]+2), np.uint8)
-            #    #cv2.floodFill(gray_roileft_eye, maskleft, (circleleft[0],circleleft[1]), (255), 10, 6)
-            #
-            #    comb = combineEyes(
-            #                        gray_roileft_eye,
-            #                        gray_roiright_eye
-            #                        )
-            #    cv2.imshow('floodfilledeyes', comb)
-            
-            
-            #pdb.set_trace()
-            
-            leftthresh = gray_roileft_eye.copy()
-            #cv2.adaptiveThreshold(leftthresh, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 25, 25, leftthresh)
-            leftthresh = thresholdByPercentage(leftthresh, .075)
-            
-            rightthresh = gray_roiright_eye.copy()
-            #cv2.adaptiveThreshold(rightthresh, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 25, 25, rightthresh)
-            rightthresh = thresholdByPercentage(rightthresh, .075)
-            
-            
-            #contours, heirarchy = cv2.findContours(leftthresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-            
-            #pdb.set_trace()
-            
-            #for idx in range(len(contours)):
-            #        
-            #    clr = (randrange(0, 255), randrange(0, 255), randrange(0, 255))
-            #    cv2.drawContours(vis_roileft_eye, contours, idx, clr, -1)
-            
+            leftthresh, leftEllipseBox = processEye(eyesubrectsleft, vis_roileft_eye, gray_roileft_eye)
+            rightthresh, rightEllipseBox = processEye(eyesubrectsright, vis_roiright_eye, gray_roiright_eye)
             
             combthresh = combineEyes(leftthresh, rightthresh)
             cv2.imshow('combinedthresh', combthresh)
             
-            #combvis = combineEyes(
-            #                    vis_roileft_eye,
-            #                    vis_roiright_eye
-            #                  )
-            #cv2.imshow('colorcombinedeyes', combvis)
             
-            #pdb.set_trace()
-        
-        #pdb.set_trace()
-
-#	    mouthsubrects = detect(roi.copy(), mouthcascade)
-#	    
-#	    # remove mouth detections that overlap with eyes
-#	    remove = []
-#	    if type(mouthsubrects) != list:
-#		for i in range(mouthsubrects.shape[0]):
-#		    for rect in eyesubrects:
-#			if is_intersection(mouthsubrects[i],rect):
-#			    remove.append(i)
-#		for i in sorted(set(remove), reverse=True):
-#		    mouthsubrects = np.delete(mouthsubrects, (i), axis=0)
-#		
-#            draw_rects(vis_roi, mouthsubrects, (0, 0, 255))
+            
+            ############ Thresholding ###########
+            #
+            ## threshold left eye
+            #leftthresh = gray_roileft_eye.copy()
+            ##cv2.adaptiveThreshold(leftthresh, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 25, 25, leftthresh)
+            #leftthresh = thresholdByPercentage(leftthresh, .075)
+            #
+            ## threshold right eye
+            #rightthresh = gray_roiright_eye.copy()
+            ##cv2.adaptiveThreshold(rightthresh, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 25, 25, rightthresh)
+            #rightthresh = thresholdByPercentage(rightthresh, .075)
+            #
+            ## display combined threshes
+            #combthresh = combineEyes(leftthresh, rightthresh)
+            #cv2.imshow('combinedthresh', combthresh)
+            #
+            #
+            ############ Contours ###########
+            #
+            ## find contours from thresholded img
+            #contours, heirarchy = cv2.findContours(leftthresh.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+            #
+            #maxarea = 0
+            #maxidx = 0
+            #
+            ## find contour with largest area
+            #for idx in range(len(contours)):
+            #    a = cv2.contourArea(contours[idx])
+            #    if maxarea < a:
+            #        maxarea = a
+            #        maxidx = idx
+            #
+            #cv2.drawContours(vis_roileft_eye, contours, maxidx, (0,0,255), -1)
+            #
+            ##for idx in range(len(contours)):
+            ##        
+            ##    clr = (randrange(0, 255), randrange(0, 255), randrange(0, 255))
+            ##    cv2.drawContours(vis_roileft_eye, contours, idx, clr, -1)
+            #
+            #
+            ############ Ellipse ###########
+            #
+            ## fit ellipse to the detected contour
+            #ellipseBox = cv2.fitEllipse(contours[maxidx])
+            #cv2.ellipse(vis_roileft_eye, ellipseBox, (50, 150, 255))
+            #
+            ##pdb.set_trace()
+            #
+            ##combvis = combineEyes(
+            ##                    vis_roileft_eye,
+            ##                    vis_roiright_eye
+            ##                  )
+            ##cv2.imshow('colorcombinedeyes', combvis)
+            #
+            ##pdb.set_trace()
+    
+    except IndexError:
+        pass
 
     cv2.imshow('facedetect', vis)
     
-    return vis, gray, rects, eyesubrectsleft, eyesubrectsright
+    return vis, gray, facerect, eyesubrectleft, eyesubrectright, leftEllipseBox, rightEllipseBox
+
+
+def writeline(f,line):
+    f.write(line+'\n')
 
 
 if __name__ == '__main__':
@@ -265,7 +313,7 @@ if __name__ == '__main__':
         img = cv2.imread(source)
         processFrame(img, facecascade, eyescascade)
         
-        pdb.set_trace()
+        #pdb.set_trace()
     
     elif mode == 'video':
         
@@ -277,37 +325,35 @@ if __name__ == '__main__':
         while True:
             ret, img = cam.read()
             
-            vis, gray, rects, eyesubrectsleft, eyesubrectsright = processFrame(img, facecascade, eyescascade)
-        
+            vis, gray, facerect, eyesubrectleft, eyesubrectright, leftEllipseBox, rightEllipseBox = processFrame(img, facecascade, eyescascade)
+            
+            #pdb.set_trace()
+            
             key = cv2.waitKey(1) & 0xff
-        
+            
             if key == ord('w'):
                 cv2.imwrite(str(fileindex)+'.png',img)
-                cv2.imwrite(str(fileindex)+'.annotated.png',vis)
+                cv2.imwrite(str(fileindex)+'.vis.png',vis)
+                cv2.imwrite(str(fileindex)+'.gray.png',gray)
                 rindex = 0 
-                if (len(rects) == 0) and (len(eyesubrectsleft) == 0) and (len(eyesubrectsright) == 0) and (len(mouthsubrects) == 0):
-                        continue
-                #else
-                f = file(str(fileindex)+'.txt','w')
-                print >>f, 'For image file '+str(fileindex)+'.png:'
-                for x1,y1,x2,y2 in rects:
-                        print >>f,'face rect ',rindex,': (',x1,',',y1,') -> (',x2,',',y2,'), ',find_rect_center((x1,y1,x2,y2))
-                        rindex = rindex +1
-                rindex = 0
-                for x1,y1,x2,y2 in eyesubrectsleft:
-                        print >>f,'left eye rect ',rindex,': (',x1,',',y1,') -> (',x2,',',y2,'), ',find_rect_center((x1,y1,x2,y2))
-                        rindex = rindex + 1
-                rindex = 0
-                for x1,y1,x2,y2 in eyesubrectsright:
-                        print >>f,'right eye rect ',rindex,': (',x1,',',y1,') -> (',x2,',',y2,'), ',find_rect_center((x1,y1,x2,y2))
-                        rindex = rindex + 1
-                #rindex = 0
-                #for x1,y1,x2,y2 in mouthsubrects:
-                #        print >>f,'mouth rect ',rindex,': (',x1,',',y1,') -> (',x2,',',y2,'), ',find_rect_center((x1,y1,x2,y2))
-                #        rindex = rindex + 1
-                f.close()
-                fileindex = fileindex + 1
-            elif key == 27:
+                if facerect != None and eyesubrectleft != None and eyesubrectright != None and leftEllipseBox != None and rightEllipseBox != None:
+                    with file(str(fileindex)+'.txt','w') as f:
+                        writeline(f,'For image file %d.png:' % fileindex)
+                        # face
+                        writeline(f,'face rect: (%d, %d) -> (%d, %d)' % tuple(facerect))
+                        
+                        # left
+                        writeline(f,'left eye rect: (%d, %d) -> (%d, %d)' % tuple(eyesubrectleft))
+                        writeline(f, 'left eye ellipse: center: (%f, %f), size: (%f, %f), angle: %f' % (leftEllipseBox[0][0],leftEllipseBox[0][1],leftEllipseBox[1][0],leftEllipseBox[1][1],leftEllipseBox[2]))
+                        
+                        # right
+                        writeline(f,'right eye rect: (%d, %d) -> (%d, %d)' % tuple(eyesubrectright))
+                        writeline(f, 'right eye ellipse: center: (%f, %f), size: (%f, %f), angle: %f' % (rightEllipseBox[0][0],rightEllipseBox[0][1],rightEllipseBox[1][0],rightEllipseBox[1][1],rightEllipseBox[2]))
+                        
+                        
+                        
+                        fileindex = fileindex + 1
+            elif key == ord('q'):
                 break
             else:
                 continue
